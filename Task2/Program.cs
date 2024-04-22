9de579ec0685da7ae7759eb50c825afaaa0962b6
Natallia <tponatali@gmail.com>
2024-04-22 18:20:23 +0300

    lesson1:task2

---
+++ Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿int i1 = 5;
+int i2 = 6;
+int sum = i1 + i2;
+Console.WriteLine (sum);
+
+long l1 = 3;
+long sum2 = l1 + i2;
+Console.WriteLine (sum2);
+
+double d1 = 5.5;
+double sum3 = d1 + i2;
+Console.WriteLine (sum3);
+
+int product = i1 * i2;
+long product2 = l1 * i2;
+double product3 = d1 * i2;
+Console.WriteLine (product);
+Console.WriteLine (product2);
+Console.WriteLine (product3);
